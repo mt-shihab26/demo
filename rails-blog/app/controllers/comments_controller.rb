5098a7bd89e0cf8e3ede80d06f3700a2008f7d92
Shihab Mahamud <shihab4t@gmail.com>
2026-01-11 03:10:15 +0600

    comment delete

--- rails-blog/app/controllers/comments_controller.rb
+++ rails-blog/app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
     redirect_to @post
   end
 
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post, notice: "Comment was successfully deleted."
+  end
+
   private
     def set_post
       @post = Post.find(params[:post_id])
