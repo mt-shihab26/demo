26ae121cc7b0ce37959d41eb4b5c74e25f1d84cf
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 20:07:54 +0600

    livewire pages

---
+++ livewire-blog/resources/views/pages/welcome.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+new #[Layout('layouts.app', ['title' => 'Hello World'])] class extends Component
+{
+    //
+};
+?>
+
+<div class="flex items-center flex-col justify-center h-screen">
+    <livewire:counter />
+</div>
