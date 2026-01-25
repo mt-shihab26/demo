bcf7a7f2fd6e15bdc919a70d8f263eebfd6c795d
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 21:53:26 +0600

    todos

--- livewire-blog/resources/views/components/todos.blade.php
+++ livewire-blog/resources/views/components/todos.blade.php
@@ -11,15 +11,15 @@ new class extends Component
 };
 ?>
 
-<div>
-    <div>
-        <input type="test" />
-        <button type="button">Add</button>
+<div class="max-w-md mx-auto p-4">
+    <div class="flex gap-2 mb-4">
+        <input type="test" class="flex-1 px-3 rounded-none py-2 border border-gray-300" />
+        <button type="button" class="px-4 py-2 bg-black text-white">Add</button>
     </div>
 
-    <ul>
+    <ul class="space-y-1">
         @foreach($todos as $todo)
-            <li>{{ $todo }}</li>
+            <li class="px-3 py-2 border-b border-gray-200">{{ $todo }}</li>
         @endforeach
     </ul>
 </div>
