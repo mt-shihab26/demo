23bd664ce7bda3935d050163cd024ca3b5f5ca91
Shihab Mahamud <shihab4t@gmail.com>
2026-01-26 03:48:05 +0600

    todos

--- livewire-blog/resources/views/components/todos.blade.php
+++ livewire-blog/resources/views/components/todos.blade.php
@@ -21,15 +21,18 @@ new class extends Component
 };
 ?>
 
-<div class="max-w-md mx-auto p-4">
+<div class="max-w-md mx-auto p-4 space-y-4">
+    <div>Current Todo: {{ $todo }}</div>
+
     <form
-        class="flex gap-2 mb-4"
+        class="flex gap-2"
         wire:submit="add"
     >
         <input
             type="test"
             class="flex-1 px-3 rounded-none py-2 border border-gray-300"
-            wire:model="todo"
+            {{-- wire:model.live.debounce.5ms="todo" --}}
+            wire:model.change="todo"
         />
         <button
             type="submit"
