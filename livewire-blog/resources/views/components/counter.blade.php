ffb5c9e15b8be21c378575836f3a05183edf4c52
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 21:46:14 +0600

    livewire actions

--- livewire-blog/resources/views/components/counter.blade.php
+++ livewire-blog/resources/views/components/counter.blade.php
@@ -10,10 +10,16 @@ new class extends Component
     {
         $this->count += $by;
     }
+
+    public function decrement(int $by)
+    {
+        $this->count -= $by;
+    }
 };
 ?>
 
-<div>
+<div class="flex flex-col">
     <div>Count: {{ $count }} </div>
-    <button type="button" wire:click="increment(2)">Increment By 2</button>
+    <button class="cursor-pointer" type="button" wire:click="increment(2)">Increment By 2</button>
+    <button class="cursor-pointer"  type="button" wire:click="decrement(2)">Decrement By 2</button>
 </div>
