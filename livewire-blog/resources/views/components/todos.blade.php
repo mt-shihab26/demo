1e0d4bef6214e320d6a653fc2b9b793cc6083ec6
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 22:01:45 +0600

    todos

--- livewire-blog/resources/views/components/todos.blade.php
+++ livewire-blog/resources/views/components/todos.blade.php
@@ -4,18 +4,40 @@ use Livewire\Component;
 
 new class extends Component
 {
+    public string $todo = '';
+
     public $todos = [
         'Take out trash',
         'Do dishes',
     ];
+
+    public function add()
+    {
+        $this->todos[] = $this->todo;
+
+        /* $this->todo = ''; */
+        $this->reset('todo');
+    }
 };
 ?>
 
 <div class="max-w-md mx-auto p-4">
-    <div class="flex gap-2 mb-4">
-        <input type="test" class="flex-1 px-3 rounded-none py-2 border border-gray-300" />
-        <button type="button" class="px-4 py-2 bg-black text-white">Add</button>
-    </div>
+    <form
+        class="flex gap-2 mb-4"
+        wire:submit="add"
+    >
+        <input
+            type="test"
+            class="flex-1 px-3 rounded-none py-2 border border-gray-300"
+            wire:model="todo"
+        />
+        <button
+            type="submit"
+            class="px-4 py-2 bg-black text-white"
+        >
+            Add
+        </button>
+    </form>
 
     <ul class="space-y-1">
         @foreach($todos as $todo)
