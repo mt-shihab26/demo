fdfa7a6329e7c8082c7bbec63efcdb72d3caac15
Shihab Mahamud <shihab4t@gmail.com>
2026-01-27 08:37:23 +0600

    posts

--- livewire-blog/resources/views/pages/posts.blade.php
+++ livewire-blog/resources/views/pages/posts.blade.php
@@ -58,9 +58,13 @@ new class extends Component
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $post['id'] }}</td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $post['title'] }}</td>
                         <td class="px-6 py-4 text-sm text-gray-500 max-w-md truncate">{{ $post['content'] }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-2">
-                            <button class="text-blue-600 hover:text-blue-900">Edit</button>
-                            <button class="text-red-600 hover:text-red-900">Delete</button>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <button class="text-blue-600 hover:text-blue-900 p-1" title="Edit">
+                                <x-icons.edit class="w-5 h-5" />
+                            </button>
+                            <button class="text-red-600 hover:text-red-900 p-1" title="Delete">
+                                <x-icons.delete class="w-5 h-5" />
+                            </button>
                         </td>
                     </tr>
                 @endforeach
