236973d9a781e90e0992e4b0b9b474cc9e1d5af1
Shihab Mahamud <shihab4t@gmail.com>
2026-01-27 09:33:40 +0600

    create post

--- livewire-blog/resources/views/components/posts/create.blade.php
+++ livewire-blog/resources/views/components/posts/create.blade.php
@@ -30,20 +30,20 @@ new class extends Component
                     <x-icons.close class="w-5 h-5" />
                 </button>
             </div>
-            <form class="p-4 flex flex-col">
-                <label>
-                    <span>Title</span>
-                    <input type="text" />
+            <form class="p-4 space-y-4">
+                <label class="block">
+                    <span class="block text-sm font-medium text-gray-700 mb-1">Title</span>
+                    <input type="text" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none" />
                 </label>
-                <label>
-                    <span>Content</span>
-                    <textarea></textarea>
+                <label class="block">
+                    <span class="block text-sm font-medium text-gray-700 mb-1">Content</span>
+                    <textarea rows="4" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none resize-none"></textarea>
                 </label>
                 <div class="flex justify-end gap-3 p-4">
-                    <button wire:click="toggle" class="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg">
+                    <button wire:click="toggle" type="button" class="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg">
                         Cancel
                     </button>
-                    <button class="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg">
+                    <button type="submit" class="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg">
                         Create Post
                     </button>
                 </div>
