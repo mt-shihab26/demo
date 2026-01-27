9edc2d33046547bd9bcfc79fe953f4b1096bb07e
Shihab Mahamud <shihab4t@gmail.com>
2026-01-27 11:07:02 +0600

    test

--- livewire-blog/resources/views/components/screens/posts/create.test.php
+++ livewire-blog/resources/views/components/screens/posts/create.test.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Livewire\Livewire;
+
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertNull;
 
 it('renders successfully', function () {
     Livewire::test('screens.posts.create')
         ->assertStatus(200);
+});
+
+it('can create new post', function () {
+    User::factory()->create();
+
+    $post = Post::whereTitle('Some title')->first();
+    assertNull($post);
+
+    Livewire::test('screens.posts.create')
+        ->set('title', 'Some title')
+        ->set('content', 'Some content')
+        ->call('save');
+
+    $post = Post::whereTitle('Some title')->first();
+
+    assertNotNull($post);
+
+    assertEquals('Some title', $post->title);
+    assertEquals('Some content', $post->content);
 });
