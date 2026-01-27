7e72ccf5e0523b042b0d0b4c67f1893433dcbbb2
Shihab Mahamud <shihab4t@gmail.com>
2026-01-27 09:45:05 +0600

    create posts

--- livewire-blog/resources/views/components/posts/create.blade.php
+++ livewire-blog/resources/views/components/posts/create.blade.php
@@ -4,28 +4,35 @@ use Livewire\Component;
 
 new class extends Component
 {
-    public bool $show = true;
-
-    public function toggle()
-    {
-        $this->show = ! $this->show;
-    }
+    //
 };
 ?>
 
-<div>
+<div x-data="{ show: false }">
     <button
-        wire:click="toggle"
+        @click="show = !show"
         class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200"
     >
         + Add Post
     </button>
-    <div wire:show="show" class="fixed inset-0 z-50 flex items-center justify-center">
-        <div class="fixed inset-0 bg-black/50" wire:click="toggle"></div>
+    <div
+        class="fixed inset-0 z-50 flex items-center justify-center"
+        x-show="show"
+        x-cloak
+        x-transition
+    >
+        <div
+            class="fixed inset-0 bg-black/50"
+            @click="show = !show"
+        >
+        </div>
         <div class="relative bg-white rounded-lg shadow-xl w-full max-w-lg">
             <div class="flex items-center justify-between p-4">
                 <h3 class="text-lg font-semibold text-gray-900">Create Post</h3>
-                <button wire:click="toggle" class="text-gray-400 hover:text-gray-600">
+                <button
+                    class="text-gray-400 hover:text-gray-600"
+                    @click="show = !show"
+                >
                     <x-icons.close class="w-5 h-5" />
                 </button>
             </div>
@@ -39,10 +46,17 @@ new class extends Component
                     <textarea rows="4" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none resize-none"></textarea>
                 </label>
                 <div class="flex justify-end gap-3 p-4">
-                    <button wire:click="toggle" type="button" class="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg">
+                    <button
+                        type="button"
+                        class="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg"
+                        @click="show = !show"
+                    >
                         Cancel
                     </button>
-                    <button type="submit" class="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg">
+                    <button
+                        type="submit"
+                        class="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg"
+                    >
                         Create Post
                     </button>
                 </div>
