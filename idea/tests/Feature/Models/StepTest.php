df95d6a3bb4d03a51eea4b77ee28055dda1a1547
Shihab Mahamud <shihab4t@gmail.com>
2026-02-04 18:01:44 +0600

    step test

--- idea/tests/Feature/Models/StepTest.php
+++ idea/tests/Feature/Models/StepTest.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Models\Idea;
 use App\Models\Step;
 
 test('is data casting', function () {
     $step = Step::factory()->create();
 
     expect($step->completed)->toBeBool();
+});
+
+test('it belongs to idea', function () {
+    $step = Step::factory()->create();
+
+    expect($step->idea)->toBeInstanceOf(Idea::class);
 });
