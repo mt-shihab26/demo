cca914eab3c7349115742645250af959aaff9e55
Shihab Mahamud <shihab4t@gmail.com>
2026-03-17 08:02:27 +0600

    poll

--- fourat/src/bin/client.rs
+++ fourat/src/bin/client.rs
@@ -1,3 +1,18 @@
+use std::time::Duration;
+
+use crossterm::event::{poll, read};
+
 fn main() {
-    println!("Hello");
+    loop {
+        while poll(Duration::ZERO).unwrap() {
+            match read().unwrap() {
+                crossterm::event::Event::FocusGained => todo!(),
+                crossterm::event::Event::FocusLost => todo!(),
+                crossterm::event::Event::Key(key_event) => todo!(),
+                crossterm::event::Event::Mouse(mouse_event) => todo!(),
+                crossterm::event::Event::Paste(_) => todo!(),
+                crossterm::event::Event::Resize(_, _) => todo!(),
+            }
+        }
+    }
 }
