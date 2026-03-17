67ad89151f5c788291f4a73c6e2599a904fb6a2b
Shihab Mahamud <shihab4t@gmail.com>
2026-03-17 08:31:31 +0600

    crossterm

--- fourat/src/bin/client.rs
+++ fourat/src/bin/client.rs
@@ -1,29 +1,25 @@
 use std::time::Duration;
 
-use crossterm::{
-    event::{Event, poll, read},
-    terminal,
-};
+use crossterm::{event, terminal};
 
 fn main() {
     let (mut width, mut height) = terminal::size().unwrap();
 
     loop {
-        while poll(Duration::ZERO).unwrap() {
+        while event::poll(Duration::ZERO).unwrap() {
             handle_events(&mut width, &mut height);
         }
-
         render_frame();
     }
 }
 
 fn handle_events(width: &mut u16, height: &mut u16) {
-    match read().unwrap() {
-        Event::Resize(w, h) => {
+    match event::read().unwrap() {
+        event::Event::Resize(w, h) => {
             *width = w;
             *height = h;
         }
-        Event::Key(_) => todo!(),
+        event::Event::Key(_) => todo!(),
         _ => (),
     }
 }
