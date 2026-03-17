b880f56a8da4298e82ee6cf13ad77898318d6dcb
Shihab Mahamud <shihab4t@gmail.com>
2026-03-17 08:22:14 +0600

    handle events

--- fourat/src/bin/client.rs
+++ fourat/src/bin/client.rs
@@ -1,18 +1,33 @@
 use std::time::Duration;
 
-use crossterm::event::{poll, read};
+use crossterm::{
+    event::{Event, poll, read},
+    terminal,
+};
 
 fn main() {
+    let (mut width, mut height) = terminal::size().unwrap();
+
     loop {
         while poll(Duration::ZERO).unwrap() {
-            match read().unwrap() {
-                crossterm::event::Event::FocusGained => todo!(),
-                crossterm::event::Event::FocusLost => todo!(),
-                crossterm::event::Event::Key(key_event) => todo!(),
-                crossterm::event::Event::Mouse(mouse_event) => todo!(),
-                crossterm::event::Event::Paste(_) => todo!(),
-                crossterm::event::Event::Resize(_, _) => todo!(),
-            }
+            handle_events(&mut width, &mut height);
         }
+
+        render_frame();
     }
+}
+
+fn handle_events(width: &mut u16, height: &mut u16) {
+    match read().unwrap() {
+        Event::Resize(w, h) => {
+            *width = w;
+            *height = h;
+        }
+        Event::Key(_) => todo!(),
+        _ => (),
+    }
+}
+
+fn render_frame() {
+    //
 }
