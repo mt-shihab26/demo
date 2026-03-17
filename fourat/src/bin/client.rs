333b1c82f1c132f5c0cf42b3981d2a1dd1a7d0fd
Shihab Mahamud <shihab4t@gmail.com>
2026-03-17 10:10:17 +0600

    client

--- fourat/src/bin/client.rs
+++ fourat/src/bin/client.rs
@@ -3,7 +3,7 @@ use std::io;
 use fourat::client::app::App;
 
 fn main() -> io::Result<()> {
-    let mut app = App::new();
+    let mut app = App::new()?;
 
     app.run()
 }
--- fourat/src/client/app.rs
+++ fourat/src/client/app.rs
@@ -11,54 +11,63 @@ use crossterm::{
     terminal,
 };
 
-pub struct App {}
+pub struct App {
+    stdout: io::Stdout,
+    width: u16,
+    height: u16,
+}
 
 impl App {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new() -> io::Result<Self> {
+        let stdout = io::stdout();
+        let (width, height) = terminal::size()?;
+
+        Ok(Self {
+            stdout,
+            width,
+            height,
+        })
     }
 
     pub fn run(&mut self) -> io::Result<()> {
-        let mut stdout = io::stdout();
-        let (mut width, mut height) = terminal::size()?;
-
         loop {
             while event::poll(Duration::ZERO)? {
-                handle_events(&mut width, &mut height)?;
+                self.handle_events()?;
             }
-            render_frame(&mut stdout)?;
+            self.render_frame()?;
             thread::sleep(Duration::from_millis(33));
         }
     }
-}
 
-fn handle_events(width: &mut u16, height: &mut u16) -> io::Result<()> {
-    match event::read()? {
-        Event::Resize(w, h) => {
-            *width = w;
-            *height = h;
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Resize(width, height) => {
+                self.width = width;
+                self.height = height;
+            }
+            Event::Key(_) => todo!(),
+            _ => (),
         }
-        Event::Key(_) => todo!(),
-        _ => (),
+
+        Ok(())
     }
 
-    Ok(())
-}
+    fn render_frame(&mut self) -> io::Result<()> {
+        self.stdout
+            .queue(terminal::Clear(terminal::ClearType::All))?;
 
-fn render_frame(stdout: &mut io::Stdout) -> io::Result<()> {
-    stdout.queue(terminal::Clear(terminal::ClearType::All))?;
-
-    for y in 0..40 {
-        for x in 0..150 {
-            if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-                stdout
-                    .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".magenta()))?;
+        for y in 0..40 {
+            for x in 0..150 {
+                if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+                    self.stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::PrintStyledContent("█".magenta()))?;
+                }
             }
         }
+
+        self.stdout.flush()?;
+
+        Ok(())
     }
-
-    stdout.flush()?;
-
-    Ok(())
 }
